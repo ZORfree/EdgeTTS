fadbf44e577f30248d16c48dcc2e02fd9896460d
ZORfree <haruihi@163.com>
2025-01-08 11:49:27 +0800

    # V0.2.2.1 code init

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace edgeTTS
+{
+    partial class edgeTTS
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(edgeTTS));
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.languageCombo = new System.Windows.Forms.ComboBox();
+            this.speedTrackBar = new System.Windows.Forms.TrackBar();
+            this.speedLabel = new System.Windows.Forms.Label();
+            this.playStopButton = new System.Windows.Forms.Button();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.savePathTextBox = new System.Windows.Forms.TextBox();
+            this.synthesizeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.allOfCheckBox = new System.Windows.Forms.CheckBox();
+            this.allOfToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.progressLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.speedTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.textBox.Location = new System.Drawing.Point(20, 20);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(540, 100);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "你好，世界";
+            // 
+            // languageCombo
+            // 
+            this.languageCombo.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.languageCombo.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.languageCombo.Location = new System.Drawing.Point(85, 138);
+            this.languageCombo.Name = "languageCombo";
+            this.languageCombo.Size = new System.Drawing.Size(343, 23);
+            this.languageCombo.TabIndex = 1;
+            this.languageCombo.SelectedIndexChanged += new System.EventHandler(this.languageCombo_SelectedIndexChanged);
+            // 
+            // speedTrackBar
+            // 
+            this.speedTrackBar.Location = new System.Drawing.Point(85, 186);
+            this.speedTrackBar.Maximum = 100;
+            this.speedTrackBar.Minimum = -100;
+            this.speedTrackBar.Name = "speedTrackBar";
+            this.speedTrackBar.Size = new System.Drawing.Size(343, 45);
+            this.speedTrackBar.TabIndex = 2;
+            this.speedTrackBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.speedTrackBar.Scroll += new System.EventHandler(this.speedTrackBar_Scroll);
+            // 
+            // speedLabel
+            // 
+            this.speedLabel.Location = new System.Drawing.Point(17, 191);
+            this.speedLabel.Name = "speedLabel";
+            this.speedLabel.Size = new System.Drawing.Size(80, 20);
+            this.speedLabel.TabIndex = 3;
+            this.speedLabel.Text = "变速: 0";
+            // 
+            // playStopButton
+            // 
+            this.playStopButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.playStopButton.Location = new System.Drawing.Point(460, 186);
+            this.playStopButton.Name = "playStopButton";
+            this.playStopButton.Size = new System.Drawing.Size(100, 25);
+            this.playStopButton.TabIndex = 4;
+            this.playStopButton.Text = "试听";
+            this.playStopButton.UseVisualStyleBackColor = false;
+            this.playStopButton.Click += new System.EventHandler(this.playStopButton_Click);
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(460, 240);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(100, 25);
+            this.browseButton.TabIndex = 5;
+            this.browseButton.Text = "浏览...";
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // savePathTextBox
+            // 
+            this.savePathTextBox.Location = new System.Drawing.Point(85, 242);
+            this.savePathTextBox.Name = "savePathTextBox";
+            this.savePathTextBox.ReadOnly = true;
+            this.savePathTextBox.Size = new System.Drawing.Size(343, 23);
+            this.savePathTextBox.TabIndex = 6;
+            // 
+            // synthesizeButton
+            // 
+            this.synthesizeButton.Location = new System.Drawing.Point(460, 291);
+            this.synthesizeButton.Name = "synthesizeButton";
+            this.synthesizeButton.Size = new System.Drawing.Size(100, 30);
+            this.synthesizeButton.TabIndex = 7;
+            this.synthesizeButton.Text = "合成";
+            this.synthesizeButton.Click += new System.EventHandler(this.synthesizeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(17, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "保存位置";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(17, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "选择语种";
+            // 
+            // allOfCheckBox
+            // 
+            this.allOfCheckBox.AutoSize = true;
+            this.allOfCheckBox.Location = new System.Drawing.Point(460, 138);
+            this.allOfCheckBox.Name = "allOfCheckBox";
+            this.allOfCheckBox.Size = new System.Drawing.Size(91, 19);
+            this.allOfCheckBox.TabIndex = 10;
+            this.allOfCheckBox.Text = "所有发言人";
+            this.allOfToolTip.SetToolTip(this.allOfCheckBox, "合成该语种所有发言人");
+            this.allOfCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // allOfToolTip
+            // 
+            this.allOfToolTip.AutomaticDelay = 100;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(85, 300);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(343, 10);
+            this.progressBar.TabIndex = 11;
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.Location = new System.Drawing.Point(17, 298);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(66, 15);
+            this.progressLabel.TabIndex = 12;
+            this.progressLabel.Text = "合成进度";
+            // 
+            // edgeTTS
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(584, 340);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.allOfCheckBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.languageCombo);
+            this.Controls.Add(this.speedTrackBar);
+            this.Controls.Add(this.speedLabel);
+            this.Controls.Add(this.playStopButton);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.savePathTextBox);
+            this.Controls.Add(this.synthesizeButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "edgeTTS";
+            this.Text = "EdgeTTS";
+            this.Load += new System.EventHandler(this.edgeTTS_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.speedTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox;
+        private ComboBox languageCombo;
+        private TrackBar speedTrackBar;
+        private Label speedLabel;
+        private Button playStopButton;
+        private Button browseButton;
+        private TextBox savePathTextBox;
+        private Button synthesizeButton;
+        private Label label1;
+        private Label label2;
+        private CheckBox allOfCheckBox;
+        private ToolTip allOfToolTip;
+        private ProgressBar progressBar;
+        private Label progressLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Edge_tts_sharp;
+using Edge_tts_sharp.Model;
+using NAudio.Wave;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+///1.实现输入文本框、语种选择框、试听|停止按钮、保存位置(默认当前目录)
+///2.实现语速的选择变换
+///3.实现该语种的所有发言人的音频一键合成
+///4.实现格式转换位单通道16k 16bit的wav音频格式
+///5.添加语速变换和进度条
+///6.防止多次点击造成重复合成设计
+namespace edgeTTS
+{
+    public partial class edgeTTS : Form
+    {
+        Dictionary<string, string> langMappings = new Dictionary<string, string>
+        {
+            { "zh", "汉语" },{ "en", "英语" },{ "af", "南非荷兰语" },
+            { "sq", "阿尔巴尼亚" },{ "am", "阿姆哈拉语" },{ "ar", "阿拉伯语" },{ "az", "阿塞拜疆语" },{ "bn", "孟加拉语" },{ "bs", "波斯尼亚语" },
+            { "bg", "保加利亚语" },{ "my", "缅甸语" },{ "ca", "加泰罗尼亚语" },{ "hr", "克罗地亚语" },{ "cs", "捷克语" },{ "da", "丹麦语" },
+            { "nl", "荷兰语" },{ "et", "爱沙尼亚语" },{ "fil", "菲律宾语" },{ "fi", "芬兰语" },{ "fr", "法语" },{ "gl", "加利西亚语" },
+            { "ka", "格鲁吉亚语" },{ "de", "德语" },{ "el", "希腊语" },{ "gu", "古吉拉特语" },{ "he", "希伯来语" },{ "hi", "印地语" },
+            { "hu", "匈牙利语" },{ "is", "冰岛语" },{ "id", "印尼语" },{ "ga", "爱尔兰语" },{ "it", "意大利语" },{ "ja", "日语" },
+            { "jv", "爪哇语" },{ "kn", "卡纳达语" },{ "kk", "哈萨克语" },{ "km", "高棉语" },{ "ko", "韩语" },{ "lo", "老挝语" },
+            { "lv", "拉脱维亚语" },{ "lt", "立陶宛语" },{ "mk", "马其顿语" },{ "ms", "马来语" },{ "ml", "马拉雅拉姆语" },{ "mt", "马耳他语" },
+            { "mr", "马拉地语" },{ "mn", "蒙古语" },{ "ne", "尼泊尔语" },{ "nb", "挪威语" },{ "ps", "普什图语" },{ "fa", "波斯语" },
+            { "pl", "波兰语" },{ "pt", "葡萄牙语" },{ "ro", "罗马尼亚语" },{ "ru", "俄语" },{ "sr", "塞尔维亚语" },{ "si", "僧伽罗语" },
+            { "sk", "斯洛伐克语" },{ "sl", "斯洛文尼亚语" },{ "so", "索马里语" },{ "es", "西班牙语" },{ "su", "巽他语" },{ "sw", "斯瓦希里语" },
+            { "sv", "瑞典语" },{ "ta", "泰米尔语" },{ "te", "泰卢固语" },{ "th", "泰语" },{ "tr", "土耳其语" },{ "uk", "乌克兰语" },
+            { "ur", "乌尔都语" },{ "uz", "乌兹别克语" },{ "vi", "越南语" },{ "cy", "威尔士语" },{ "zu", "祖鲁语" }
+
+        };
+        string currentDirectory = Directory.GetCurrentDirectory();
+        //private IWavePlayer _device;
+        private IWavePlayer _device = new WaveOutEvent(); // Create device
+        private CancellationTokenSource _cts;
+        
+
+
+
+        public edgeTTS()
+        {
+            InitializeComponent();
+            savePathTextBox.Text = currentDirectory;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.Text = "EdgeTTS By Zorfree V" + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        }
+
+
+        private void edgeTTS_Load(object sender, EventArgs e)
+        {
+            var voices = Edge_tts.GetVoice();
+            string defaultVoice = null;
+            foreach (var item in voices)
+            {
+                //voice name isMicrosoft Server Speech Text to Speech Voice (zu-ZA, ThembaNeural), locale（语言） is zu-ZA, SuggestedCodec(音频类型) is audio-24khz-48kbitrate-mono-mp3
+
+                //Console.WriteLine($"voice name is{item.Name}, locale（语言） is {item.Locale}, SuggestedCodec(音频类型) is {item.SuggestedCodec}");
+                char[] delimiters = new char[] { '-' };
+                string value;
+                if (langMappings.TryGetValue(item.Locale.Split(delimiters)[0], out value))
+                {
+                    string itemVoice = item.Name.Replace("Microsoft Server Speech Text to Speech Voice", value);
+                    languageCombo.Items.Add(itemVoice);
+                    if (defaultVoice == null)
+                    {
+                        if (item.Name.Contains("zh-")){
+                            defaultVoice = itemVoice;
+                        }
+                    }
+                }
+                else
+                {
+                    languageCombo.Items.Add(item.Name.Replace("Microsoft Server Speech Text to Speech Voice", "未知"));
+                }
+            }
+
+            languageCombo.SelectedIndex = languageCombo.Items.IndexOf(defaultVoice);
+        }
+
+        private void languageCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            
+        }
+        private void StartUpdateProgress()
+        {
+            // 此处可用Timer完成而不是手动循环，但不建议使用UI线程上的Timer
+            Task.Run(() =>
+            {
+                while (!_cts.IsCancellationRequested)
+                {
+                    Console.WriteLine("状态检测当前播放状态"+_device.PlaybackState);
+                    if (_device.PlaybackState == PlaybackState.Stopped)
+                    {
+                        BeginInvoke(new Action(UpdateProgress));
+
+                        Thread.Sleep(100);
+                    }
+                    else
+                    {
+                        Thread.Sleep(100);
+                    }
+                }
+            });
+        }
+        private void UpdateProgress()
+        {
+            playStopButton.Text = "试听";
+            playStopButton.BackColor = Color.Green;
+            textBox.Enabled = true;
+            DisposeAll();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.Description = "请选择合成音频保存的目录";
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                string path = folder.SelectedPath; // "e:/go"
+                savePathTextBox.Text = path;
+            }
+        }
+
+        private void playStopButton_Click(object sender, EventArgs e)
+        {
+            
+
+            if (_device.PlaybackState == PlaybackState.Playing)
+            {
+                StopAction();
+                //DisposeAll();
+
+            }
+            else
+            {
+                playStopButton.Text = "停止";
+                playStopButton.BackColor = Color.Red;
+                textBox.Enabled = false;
+                _cts = new CancellationTokenSource();
+                
+                char[] delimiters = new char[] { '(' };
+                var voice = Edge_tts.GetVoice().FirstOrDefault(i => i.Name.Contains(languageCombo.Text.Split(delimiters)[1]));
+                string msg = textBox.Text.Replace("#"," ");
+                if (voice != null)
+                {
+                    if (!string.IsNullOrWhiteSpace(msg))
+                    {
+                        //Edge_tts.PlayText(msg, voice);
+                        // 文字转语音，并且设置语速
+                        //Edge_tts.PlayText(msg, voice, speedTrackBar.Value);
+                        // 回调函数的第一个参数是binary数据
+                        PlayOption option = new PlayOption
+                        {
+                            Rate = speedTrackBar.Value,
+                            Text = msg,
+                        };
+                        Edge_tts.Invoke(option, voice, (_binary) =>
+                        {
+                            //Console.WriteLine(_device.PlaybackState);
+
+                            var ms = new MemoryStream(_binary.ToArray());
+                            var sr = new StreamMediaFoundationReader(ms);
+                            _device.Init(sr);
+                            //_device.PlaybackStopped += Device_OnPlaybackStopped;
+                            PlayAction();
+                            StartUpdateProgress(); // 界面更新线程
+
+                        });
+                    }
+                }
+            }
+
+        }
+        private void Device_OnPlaybackStopped(object obj, StoppedEventArgs arg)
+        {
+            StopAction();
+        }
+
+        private void StopAction()
+        {
+            _device?.Stop();
+        }
+
+        private void PlayAction()
+        {
+            _device?.Play();
+        }
+
+        private void PauseAction()
+        {
+            _device?.Pause();
+        }
+
+        private void DisposeDevice()
+        {
+            if (_device != null)
+            {
+                _device.PlaybackStopped -= Device_OnPlaybackStopped;
+                _device.Dispose();
+            }
+        }
+
+        private void DisposeAll()
+        {
+            DisposeDevice();
+            _cts?.Cancel();
+            _cts?.Dispose();
+        }
+
+        private void speedTrackBar_Scroll(object sender, EventArgs e)
+        {
+            //变速 : 0
+            speedLabel.Text = "变速: " + speedTrackBar.Value;
+        }
+
+        private void synthesizeButton_Click(object sender, EventArgs e)
+        {
+            textBox.Enabled = false;
+            synthesizeButton.Enabled = false;
+            string[] msgs = textBox.Text.Split(new char[] { '#' });
+            string languageComboText = languageCombo.Text;
+            int speedTrackBarValue = speedTrackBar.Value;
+            string savePathText = savePathTextBox.Text;
+            string speed_type = "normal";
+
+            if (speedTrackBarValue > 20 )
+            {
+                speed_type = "fast";
+
+            }else if (speedTrackBarValue < -20)
+            {
+                speed_type = "slow";
+            }
+
+            String outputPath = Path.Combine(savePathText, "Output").ToString();
+
+            new Thread(() =>
+            {
+                
+                if (!Directory.Exists(outputPath))
+                {
+                    Directory.CreateDirectory(outputPath);
+                }
+
+                TextWriter wav_scp = new StreamWriter(Path.Combine(outputPath, "edge_tts.txt"));
+                TextWriter wav_lab = new StreamWriter(Path.Combine(outputPath, "edge_tts.lab"));
+                for (int j = 0; j < msgs.Length; j++)
+                {
+                    string msg = msgs[j];
+
+                    if (!string.IsNullOrWhiteSpace(msg))
+                    {
+
+                        DateTime now = DateTime.Now;
+                        string nowString = now.ToString().Replace(" ", "-").Replace("/", "-").Replace(":", "_");
+                        string savePath = Path.Combine(outputPath, nowString).ToString();
+
+                        if (!Directory.Exists(savePath))
+                        {
+                            Directory.CreateDirectory(savePath);
+                        }
+                        if (allOfCheckBox.Checked)
+                        {
+                            char[] delimiters = new char[] { '(', '-', ')' };
+                            Console.WriteLine(languageComboText.Split(delimiters)[1]);
+                            var voices = Edge_tts.GetVoice().FindAll(i => i.Name.Contains(languageComboText.Split(delimiters)[1]+"-"));
+                            int count = voices.Count;
+                            BeginInvoke(new Action(() => { progressBar.Maximum = count; }));
+
+                            int N = 0;
+                            foreach (var item in voices)
+                            {
+                                
+                                char[] t_delimiters = new char[] { '(', ')' };
+                                string saveName = $"{item.Name.Split(t_delimiters)[1].Replace(",", "_").Replace(" ", "") + j.ToString()}.wav";
+
+                                //Console.WriteLine($"voice name is{item.Name}, locale（语言） is {item.Locale}, SuggestedCodec(音频类型) is {item.SuggestedCodec}");
+                                // 只保存，不播放
+                                //Edge_tts.SaveAudio(msg, item, speedTrackBarValue, Path.Combine(savePath, $"{saveName}.mp3"));
+                                PlayOption option = new PlayOption
+                                {
+                                    Rate = speedTrackBarValue,
+                                    Text = msg,
+                                };
+                                Edge_tts.Invoke(option, item, (_binary) =>
+                                {
+                                    var outFile = Path.Combine(savePath, saveName);
+                                    var ms = new MemoryStream(_binary.ToArray());
+                                    var reader = new StreamMediaFoundationReader(ms);
+                                    var outFormat = new WaveFormat(16000, 1);
+                                    var resampler = new MediaFoundationResampler(reader, outFormat);
+                                    WaveFileWriter.CreateWaveFile(outFile, resampler);
+                                });
+                                wav_scp.WriteLine(nowString + "/" + saveName);
+                                wav_lab.WriteLine(saveName + "\t" + speed_type + "\t" + msg + "\tpinyin\t1");
+
+                                N++;
+                                BeginInvoke(new Action(() => { progressBar.Value = N; }));
+
+                            }
+                        }
+                        else
+                        {
+                            char[] delimiters = new char[] { '(', ')' };
+                            var voice = Edge_tts.GetVoice().FirstOrDefault(i => i.Name.Contains(languageComboText.Split(delimiters)[1]));
+                            //string savePath = Path.Combine(outputPath, nowString).ToString();
+                            string saveName = $"{voice.Name.Split(delimiters)[1].Replace(",", "_").Replace(" ", "") + j.ToString()}.wav";
+                            PlayOption option = new PlayOption
+                            {
+                                Rate = speedTrackBarValue,
+                                Text = msg,
+                            };
+                            Edge_tts.Invoke(option, voice, (_binary) =>
+                            {
+                                var outFile = Path.Combine(savePath, saveName);
+                                var ms = new MemoryStream(_binary.ToArray());
+                                var reader = new StreamMediaFoundationReader(ms);
+                                var outFormat = new WaveFormat(16000, 1);
+                                var resampler = new MediaFoundationResampler(reader, outFormat);
+                                WaveFileWriter.CreateWaveFile(outFile, resampler);
+                            });
+                            wav_scp.WriteLine(nowString + "/" + saveName);
+                            wav_lab.WriteLine(saveName + "\t" + speed_type + "\t" + msg + "\tpinyin\t1");
+
+                            BeginInvoke(new Action(() => { progressBar.Value = progressBar.Maximum; }));
+                        }
+                    }
+
+                }
+                wav_scp.Close();
+                wav_lab.Close();
+                Console.WriteLine("已经关闭拉");
+
+
+
+                BeginInvoke(new Action(() =>
+                {
+                    textBox.Enabled = true;
+                    synthesizeButton.Enabled = true;
+                }));
+            }).Start();
+        }
+    }
+}
